b20c7110d0bba080cb3762d0d915f52024bfae51
ibtihaj_jalil <161217744+ibtihaj-jalil@users.noreply.github.com>
2025-09-19 00:14:37 +0600

    Update pop_back_LL.cpp

--- LINK_LIST/pop_back_LL.cpp
+++ LINK_LIST/pop_back_LL.cpp
@@ -1,1 +1,85 @@
 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_back(int val) // O(1)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode; // connect the old tail to the new node
+            tail = newNode;       // tail will be the new node
+        }
+    }
+    void pop_back()
+    { // O(n)
+
+        if (head == NULL)
+        {
+            cout << "List is null" << endl;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != tail) // (temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+    }
+    void print()
+    {
+        Node *ptr = head;
+        while (ptr != NULL)
+        {
+            cout << ptr->data << "-> ";
+            ptr = ptr->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    List ll;
+
+    ll.push_back(3);
+    ll.push_back(2);
+    ll.push_back(1);
+    
+    ll.pop_back();
+
+    
+    ll.print();
+    return 0;
+}

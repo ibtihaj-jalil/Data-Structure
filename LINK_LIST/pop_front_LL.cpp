4e1d640f501313e406e3d0a4bfa4107afc3e5f41
ibtihaj_jalil <161217744+ibtihaj-jalil@users.noreply.github.com>
2025-09-19 00:07:26 +0600

    Update pop_front_LL.cpp

--- LINK_LIST/pop_front_LL.cpp
+++ LINK_LIST/pop_front_LL.cpp
@@ -1,1 +1,65 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int val)//O(1)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head; // establish connection with the head node
+            head = newNode;       // head will be the new node
+        }
+    }
+
+    void print()
+    {
+        Node *ptr = head;
+        while (ptr != NULL)
+        {
+            cout << ptr->data << "-> ";
+            ptr = ptr->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+
+
+    ll.print();
+
+    return 0;
+}
